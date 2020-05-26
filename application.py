2e11db952ed15673692cfb2dddf4a46e7b4e5dc3
Pacil142857 <7coolaustin7@gmail.com>
2020-05-26 13:43:38 +0000

    Added a function to log in

--- application.py
+++ application.py
@@ -22,7 +22,7 @@ def check_pass(username, password):
     c = conn.cursor()
     # Get the user's hashed password and their salt
     key, salt = [i for i in c.execute('SELECT password, salt FROM users WHERE username=?;', (username,))][0]
-    new_key = pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 832379)
+    new_key = pbkdf2_hmac('sha256', password, salt, 832379)
     conn.close()
 
     # If the password matches, return True. Otherwise, return False.
@@ -57,9 +57,17 @@ def make_table(username):
     conn.close()
 
 
+"""# testing delete this comment later
+conn = sqlite3.connect('gpa.db')
+c = conn.cursor()
+x = hash('gamer1')
+c.execute('INSERT INTO users (username, password, salt) VALUES (?, ?, ?)', ('admin1', x[0], x[1]))
+conn.commit()
+conn.close()
+"""
 # Below is Flask stuff. Comment it out if you want to try something without the HTML.
 
-# This function allows me to require the user to be logged in
+# Allows me to require the user to be logged in
 def login_required(f):
     '''
     Decorate routes to require login.
@@ -72,6 +80,12 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+
+# Returns a page with an error message
+def apology(message, code=400):
+    '''Render message as an apology to user.'''
+    return render_template('apology.html', top=code, bottom=message), code
 
 
 # No idea what any of this does
@@ -95,5 +109,56 @@ def after_request(response):
 
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''Log user in'''
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == 'POST':
+
+        # Ensure username was submitted
+        if not request.form.get('username'):
+            return apology('Must provide username')
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Must provide password")
+
+        conn = sqlite3.connect('gpa.db')
+        c = conn.cursor()
+
+        # Query database for username
+        data = c.execute('SELECT * FROM users WHERE username = ?', (request.form.get('username'),))
+
+        # Get the data by itself
+        for i in data:
+            data = i
+
+        if type(data) == sqlite3.Cursor:
+            conn.close()
+            return apology('Invalid username and/or password')
+
+        # Ensure username exists and password is correct
+        if not check_pass(data[1], request.form.get('password').encode('utf-8')):
+            conn.close()
+            return apology('Invalid username and/or password')
+
+        # Forget any user_id
+        session.clear()
+
+        # Remember which user has logged in
+        session['user_id'] = data[0]
+
+        conn.close()
+        # Redirect user to home page
+        return redirect('/')
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template('login.html')
+
